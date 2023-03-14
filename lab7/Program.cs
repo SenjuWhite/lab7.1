e9824e4449699dad6b2cedba1463700f84e93e6a
SenjuWhite <nikitaskraga6@gmail.com>
2023-03-14 09:55:29 +0200

    40 35

--- lab7/Program.cs
+++ lab7/Program.cs
@@ -4,7 +4,7 @@ for (int i = 0; i < arr.GetLength(0); i++)
 {
     for (int j = 0; j < arr.GetLength(1); j++)
     {
-        arr[i, j] = rand.Next(25, 40);
+        arr[i, j] = rand.Next(25, 35);
         Console.Write(arr[i, j] + "\t");
     }
     Console.WriteLine();
